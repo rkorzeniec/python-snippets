0469e823c96701173800ff7c84a1b1d5e0b78e04
Robert Korzeniec <r.korzeniec@gmail.com>
2022-01-14 07:13:06 +0100

    Add initial leet code solution

---
+++ leet_code/longest_substring_without_repeating_characters.py
@@ -0,0 +1,50 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+from typing import List
+import unittest
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        scanned_chars: List[str] = []
+        result: int = 0
+
+        if len(s) <= 1:
+            return len(s)
+
+        for char in s:
+            if char in scanned_chars:
+                scanned_chars = scanned_chars[scanned_chars.index(char) + 1:]
+
+            scanned_chars.append(char)
+            result = max(result, len(scanned_chars))
+
+        return result
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.service = Solution()
+
+    def test_abcabcbb(self):
+        self.assertEqual(self.service.lengthOfLongestSubstring('abcabcbb'), 3)
+
+    def test_bbbbb(self):
+        self.assertEqual(
+            self.service.lengthOfLongestSubstring('bbbbb'), 1)
+
+    def test_pwwkew(self):
+        self.assertEqual(self.service.lengthOfLongestSubstring('pwwkew'), 3)
+
+    def test_empty(self):
+        self.assertEqual(self.service.lengthOfLongestSubstring(' '), 1)
+
+    def test_au(self):
+        self.assertEqual(self.service.lengthOfLongestSubstring('au'), 2)
+
+    def test_dvdf(self):
+        self.assertEqual(self.service.lengthOfLongestSubstring('dvdf'), 3)
+
+    def test_aabaab(self):
+        self.assertEqual(self.service.lengthOfLongestSubstring('aabaab!bb'), 3)
+
+if __name__ == "__main__":
+    unittest.main()
