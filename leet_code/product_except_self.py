de6450501b98d16ad21f75c37a7b6ad0c1f66dd5
Robert Korzeniec <r.korzeniec@gmail.com>
2022-01-20 06:08:27 +0100

    leetcode.com/problems/remove-nth-node-from-end-of-list

---
+++ leet_code/product_except_self.py
@@ -0,0 +1,120 @@
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list
+
+import enum
+from typing import List, Optional
+import unittest
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer: List[int] = []
+
+        for i in range(len(nums)):
+            product: Optional[int] = None
+
+            for j, num in enumerate(nums):
+                if i == j:
+                    continue
+
+                if product == None:
+                    product = num
+                else:
+                    product *= num
+
+            answer.append(product)
+
+        return answer
+
+
+class Solution2:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer: List[int] = [1 for i in nums]
+
+        product = 1
+        for i in range(1, len(nums)):
+            product *= nums[i - 1]
+            answer[i] = product
+
+        product = 1
+        for i in range(len(nums) - 2, -1, -1):
+            product *= nums[i + 1]
+            answer[i] *= product
+
+        return answer
+
+
+
+class Solution3:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product = 1
+        zeros_count = 0
+
+        for n in nums:
+            if n == 0:
+                zeros_count += 1
+            else:
+                product *= n
+
+        if zeros_count == 0:
+            for i in range(len(nums)):
+                nums[i] = int(product / nums[i])
+
+        elif zeros_count == 1:
+            for i in range(len(nums)):
+                if nums[i] == 0:
+                    nums[i] = product
+                else:
+                    nums[i] = 0
+
+        else:
+            nums = [0] * len(nums)
+
+        return nums
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+        self.solution2 = Solution2()
+        self.solution3 = Solution3()
+
+    def test_1(self):
+        self.assertEqual(
+            self.solution.productExceptSelf([1, 2, 3, 4]),
+            [24,12,8,6]
+        )
+
+        self.assertEqual(
+            self.solution2.productExceptSelf([1, 2, 3, 4]),
+            [24, 12, 8, 6]
+        )
+
+        self.assertEqual(
+            self.solution3.productExceptSelf([1, 2, 3, 4]),
+            [24, 12, 8, 6]
+        )
+
+    def test_2(self):
+        self.assertEqual(
+            self.solution.productExceptSelf([-1, 1, 0, -3, 3]),
+            [0, 0, 9, 0, 0]
+        )
+
+        self.assertEqual(
+            self.solution2.productExceptSelf([-1, 1, 0, -3, 3]),
+            [0, 0, 9, 0, 0]
+        )
+
+        self.assertEqual(
+            self.solution3.productExceptSelf([-1, 1, 0, -3, 3]),
+            [0, 0, 9, 0, 0]
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
