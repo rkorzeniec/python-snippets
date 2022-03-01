d3aef9bee5a62ce50c55a672022836f6cc358b8c
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-01 19:58:38 +0100

    Add missing daily byte challenge comments

--- daily_byte/5_add_binary.py
+++ daily_byte/5_add_binary.py
@@ -1,5 +1,6 @@
 """
-This question is asked by Apple. Given two binary strings (strings containing only 1s and 0s) return their sum (also as a binary string).
+This question is asked by Apple.
+Given two binary strings (strings containing only 1s and 0s) return their sum (also as a binary string).
 Note: neither binary string will contain leading 0s unless the string itself is 0
 Ex: Given the following binary strings...
     "100" + "1", return "101"
